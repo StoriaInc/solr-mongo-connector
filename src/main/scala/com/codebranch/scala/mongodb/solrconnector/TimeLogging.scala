db9eb57b30987f5201bf4f647f4bc017436ea073
Alexey Kardapoltsev <alexey.kardapoltsev@gmail.com>
2013-10-23 18:13:00 +0600

    licence text added

--- src/main/scala/com/codebranch/scala/mongodb/solrconnector/TimeLogging.scala
+++ src/main/scala/com/codebranch/scala/mongodb/solrconnector/TimeLogging.scala
@@ -1,15 +1,23 @@
+/**
+ * Copyright 2013 SelfishInc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.codebranch.scala.mongodb.solrconnector
 
 
 import akka.actor.{ActorLogging, Actor}
 
-
-
-/**
- * User: alexey
- * Date: 9/21/13
- * Time: 9:52 PM
- */
 trait TimeLogging { this : Actor with ActorLogging =>
   def loggingTime[T](action: String)(f: => T):T = {
     log.info(s"$action started")
